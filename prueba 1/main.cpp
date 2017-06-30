c7ea19eee24ffdaec037b8ee3679dbf3a94833f2
malvasan <malvasan@hotmail.com>
2017-06-29 21:38:27 -0500

    avance 2.0

--- prueba 1/main.cpp
+++ prueba 1/main.cpp
@@ -5,7 +5,7 @@
 #include<stdio.h>
 
 
-#define TAMANO      5
+#define TAMANO      10
 #define ARRIBA      72
 #define ABAJO       80
 #define IZQUIERDA   75
@@ -112,59 +112,182 @@ class Pac_man
         y=TAMANO/2;
         c='%';
     }
+    void mover(Matriz &probar)
+    {
+        int temp_x=x;
+        int temp_y=y;
+        int tecla=getch();
+        if(tecla==ARRIBA){
+            x=x-1;
+            if(probar.matriz[x][y].pasar==false)
+                x=x+1;
+            if(x<0)
+                x=0;
+        }
+        else if(tecla==ABAJO){
+            x=x+1;
+            if(probar.matriz[x][y].pasar==false)
+                x=x-1;
+            if(x>TAMANO-1)
+                x=TAMANO-1;
+        }
+
+        else if(tecla==IZQUIERDA){
+            y=y-1;
+            if(probar.matriz[x][y].pasar==false)
+                y=y+1;
+            if(y<0)
+                y=0;
+        }
+        else if(tecla==DERECHA){
+            y=y+1;
+            if(probar.matriz[x][y].pasar==false)
+               y=y-1;
+            if(y>TAMANO-1)
+                y=TAMANO-1;
+        }
+        probar.matriz[temp_x][temp_y].c='/';
+        probar.matriz[x][y].c=c;
+        probar.matriz[x][y].punto=0;
+        system("cls");
+    }
+
+};
+
+class Fantasma
+{
+    public:
+        int x;
+        int y;
+        char c;
+        Fantasma()
+        {
+            x=TAMANO/3;
+            y=TAMANO/3;
+            c='$';
+        }
+        bool chocar(Pac_man paco)
+        {
+            if(paco.x==x && paco.y==y)
+                return true;
+            else
+                return false;
+        }
+        void mover(Matriz &probar,int tecla=0)
+        {
+        int temp_x=x;
+        int temp_y=y;
+        if(tecla==0)
+            tecla=getch();
+        else
+            tecla;
+        if(tecla==ARRIBA){
+            x=x-1;
+            if(probar.matriz[x][y].pasar==false)
+                x=x+1;
+            if(x<0)
+                x=0;
+        }
+        else if(tecla==ABAJO){
+            x=x+1;
+            if(probar.matriz[x][y].pasar==false)
+                x=x-1;
+            if(x>TAMANO-1)
+                x=TAMANO-1;
+        }
+
+        else if(tecla==IZQUIERDA){
+            y=y-1;
+            if(probar.matriz[x][y].pasar==false)
+                y=y+1;
+            if(y<0)
+                y=0;
+        }
+        else if(tecla==DERECHA){
+            y=y+1;
+            if(probar.matriz[x][y].pasar==false)
+               y=y-1;
+            if(y>TAMANO-1)
+                y=TAMANO-1;
+        }
+        if(probar.matriz[temp_x][temp_y].punto==1)
+            probar.matriz[temp_x][temp_y].c='-';
+        else
+            probar.matriz[temp_x][temp_y].c='/';
+        probar.matriz[x][y].c=c;
+        }
+        void cazar(Pac_man paco,Matriz &probar)
+        {
+            if (paco.x==x){
+                if(paco.y<y){
+                    if(probar.matriz[x][y-1].pasar==false){
+                        if(x-1<0)
+                            mover(probar,ABAJO);
+                        else if(x+1>TAMANO)
+                            mover(probar,ARRIBA);
+                    }
+                    else
+                        mover(probar,IZQUIERDA);
+
+                }
+                else{
+                    if(probar.matriz[x][y+1].pasar==false){
+                        if(x-1<0)
+                            mover(probar,ABAJO);
+                        else if(x+1>TAMANO)
+                            mover(probar,ARRIBA);
+                    }
+                    else
+                        mover(probar,DERECHA);
+                }
+            }
+            else if(paco.y==y){
+                if(paco.x<x){
+                    if(probar.matriz[x-1][y].pasar==false){
+                        if(y-1<0)
+                            mover(probar,DERECHA);
+                        else if(y+1>TAMANO)
+                            mover(probar,IZQUIERDA);
+                    }
+                    else
+                        mover(probar,ARRIBA);
+
+                }
+                else{
+                    if(probar.matriz[x+1][y].pasar==false){
+                        if(y-1<0)
+                            mover(probar,DERECHA);
+                        else if(y+1>TAMANO)
+                            mover(probar,IZQUIERDA);
+                    }
+                    else
+                        mover(probar,ABAJO);
+                }
+            }
+        }
+
 };
 
 int main()
 {
     Pac_man paco;
+    Fantasma fanta;
     Matriz probar;
     probar.matriz[paco.x][paco.y].c=paco.c;
+    probar.matriz[fanta.x][fanta.y].c=fanta.c;
     bool game_over= false;
+    probar.imprimir_matriz();
     while(!game_over)
     {
-        if(kbhit())
-        {
-            int temp_x=paco.x;
-            int temp_y=paco.y;
-            char tecla=getch();
-            if(tecla=='i'){
-                paco.x=paco.x-1;
-                if(probar.matriz[paco.x][paco.y].pasar==false)
-                    paco.x=paco.x+1;
-                if(paco.x<0)
-                    paco.x=0;
-            }
-            else if(tecla=='k'){
-                paco.x=paco.x+1;
-                if(probar.matriz[paco.x][paco.y].pasar==false)
-                    paco.x=paco.x-1;
-                if(paco.x>TAMANO-1)
-                    paco.x=TAMANO-1;
-            }
-
-            else if(tecla=='j'){
-                paco.y=paco.y-1;
-                if(probar.matriz[paco.x][paco.y].pasar==false)
-                    paco.y=paco.y+1;
-                if(paco.y<0)
-                    paco.y=0;
-            }
-            else if(tecla=='l'){
-                paco.y=paco.y+1;
-                if(probar.matriz[paco.x][paco.y].pasar==false)
-                    paco.y=paco.y-1;
-                if(paco.y>TAMANO-1)
-                    paco.y=TAMANO-1;
-            }
-            probar.matriz[temp_x][temp_y].c='/';
-            probar.matriz[paco.x][paco.y].c=paco.c;
-            probar.matriz[paco.x][paco.y].punto=0;
-        }
+        paco.mover(probar);
+        fanta.cazar(paco,probar);
         probar.imprimir_matriz();
         cout<<paco.x<<" "<<paco.y<<endl;
-        Sleep(200);
-        system("cls");
+        Sleep(100);
         game_over=probar.ganar();
+        if(game_over==true)
+            break;
+        game_over=fanta.chocar(paco);
     }
 
 
